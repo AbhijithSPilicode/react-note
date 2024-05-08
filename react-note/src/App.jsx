59685e29635ed7833e640b39df41e20c22f729bc
AbhijithSPilicode <abhijithspilicode2000@gmail.com>
2024-05-08 23:46:36 +0530

    edited

---
+++ react-note/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import EditorNote from './assets/components/Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <EditorNote/>
+    </div>
+  );
+}
+
+export default App;
